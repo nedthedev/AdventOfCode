a1f799572223b3542367d6475eab5ff22cbdcef2
nedthedev <39634025+nedthedev@users.noreply.github.com>
2024-10-28 19:04:38 -0500

    day 17 done

--- 2015/Day17/README.md
+++ 2015/Day17/README.md
@@ -1,5 +1,7 @@
 # [2015 - Day 17 (Link)](https://adventofcode.com/2015/day/17)
 
 ## Part One
+I spent a good amount of time thinking about the best approach to this one. I ended up really liking how I went about it. What I did, was find every possible combination of containers by doing 2^(length of containers), then stepping through that range and converting the integer to binary to get a string representation of 1 or 0, for whether or not we use the container or not. If, when stepping through the binary, the value is 1, then add that container to the sum. If by the end of the iterating the sum is the same as the volume of eggnog then we know it's a valid combination.
 
 ## Part Two
+Used the same logic as part one, but it was a matter of finding the combination that required the fewest number of containers. Not too bad, same iterating, just a matter of keeping an additional variable that would monitor whether or not the number of containers is a new minimum or if it matches the current minimum.
--- 2015/Day17/main.py
+++ 2015/Day17/main.py
@@ -1,12 +1,79 @@
 #!/usr/bin/python3
 
-DATA = """"""
+DATA = """50
+44
+11
+49
+42
+46
+18
+32
+26
+40
+21
+7
+18
+43
+10
+47
+36
+24
+22
+40""".split("\n")
 
-def part_one(data):
-	pass
+TEST = """20
+15
+10
+5
+5""".split("\n")
 
-def part_two(data):
-	pass
+def get_combinations(containers):
+	combos = []
+	bin = ''
+	for i in range(1, 2**(len(containers))):
+		bin = '{0:b}'.format(i)
+		if(len(bin) < len(containers)+1):
+			bin = ('0' * (len(containers) - len(bin))) + bin
+		combos.append(bin)
+	return combos
+
+def parse(data):
+	parsed = []
+	for size in data:
+		parsed.append(int(size))
+	parsed.sort(reverse=True)
+	return parsed
+
+def part_one(data, combinations, liters):
+	tmp = valid = 0
+	for combination in combinations:
+		tmp = 0
+		for i, container in enumerate(combination):
+			if(container == '1'):
+				tmp += data[i]
+		if(tmp == liters):
+			valid += 1
+	return valid
+
+def part_two(data, combinations, liters):
+	tmp = valid = containers = 0
+	min = len(data)
+	for combination in combinations:
+		tmp = containers = 0
+		for i, container in enumerate(combination):
+			if(container == '1'):
+				containers += 1
+				tmp += data[i]
+		if(tmp == liters):
+			if(containers == min):
+				valid += 1
+			elif(containers < min):
+				min = containers
+				valid = 1
+	return valid
 
 if __name__ == "__main__":
-	print("Hi :)")
+	data = parse(DATA)
+	combinations = get_combinations(data)
+	print(part_one(data, combinations, 150))
+	print(part_two(data, combinations, 150))
