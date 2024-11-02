b2a479f4d8941f5ed811ebccad221026769ca55d
nedthedev <39634025+nedthedev@users.noreply.github.com>
2024-11-01 22:34:57 -0500

    day 20 done

--- 2015/Day20/main.py
+++ 2015/Day20/main.py
@@ -1,12 +1,34 @@
 #!/usr/bin/python3
 
-DATA = """"""
+import time
+import math
 
-def part_one(data):
-	pass
+DATA = """36000000"""
 
-def part_two(data):
-	pass
+TEST = """360"""
+
+def part_one(data, pkg_per):
+	delivered = pkg_per
+	house = 0
+	while(delivered < int(data)):
+		house += 1
+		delivered = house * pkg_per
+		for i in range(1, house):
+			if(house % i == 0):
+				delivered += (i * pkg_per)
+	return house
+
+def part_two(data, pkg_per):
+	delivered = pkg_per
+	house = 0
+	while(delivered < int(data)):
+		house += 1
+		delivered = house * pkg_per
+		for i in range(math.ceil(house/50), house):
+			if(house % i == 0):
+				delivered += (i * pkg_per)
+	return house
 
 if __name__ == "__main__":
-	print("Hi :)")
+	print(part_one(DATA, 10))
+	print(part_two(DATA, 11))
